a7d470e3be94e79dcba53418ba54c409c5484144
Kevin Arias <aa-kevin@hotmail.com>
2024-03-01 19:07:30 -0600

    size screen our world fixed

--- src/components/ourWorld/MedalsItemSlider.tsx
+++ src/components/ourWorld/MedalsItemSlider.tsx
@@ -32,9 +32,9 @@ export const MedalsItemSlider = ({ rumMedals, lastOne }: Props) => {
   };
 
   return (
-    <div className="flex flex-row justify-start w-full min-w-max">
+    <div className={`flex flex-row justify-start w-full min-w-max ${lastOne ? 'sm:h-md:mr-96 mr-56' :''}`}>
       <div className="flex">
-        <img src={rumMedals.img} alt="" className="mt-6 sm:h-md:w-[21rem] sm:w-[18rem] sm:h-fit" />
+        <img src={rumMedals.img} alt="" className="mt-6 sm:h-md:w-[21rem] sm:w-[18rem] sm:h-fit h-144" />
       </div>
       <div className="flex flex-col">
         {rumMedals.medals.map((med, i) => (
@@ -49,7 +49,9 @@ export const MedalsItemSlider = ({ rumMedals, lastOne }: Props) => {
       </div>
       <div className="flex flex-col justify-start items-center ml-8">
         <img src={medalData.img} alt={medalData.name} className="sm:h-md:w-60 w-44 h-fit" />
-        <p className="w-80 mt-5 text-green-300 text-lg font-normal text-justify sm:h-md:h-[340px] h-[300px] overflow-y-auto pr-1">{t(medalData.text)} </p>
+        <p className="w-80 mt-5 text-green-300 text-lg font-normal text-justify sm:h-md:h-[340px] h-[300px] overflow-y-auto pr-1">
+          {t(medalData.text)}{' '}
+        </p>
         <a
           href="https://roncentenario.blob.core.windows.net/multimedia/Book of medals 2023 (12 premium).pdf"
           target="_blank"
@@ -63,9 +65,11 @@ export const MedalsItemSlider = ({ rumMedals, lastOne }: Props) => {
           <span>{t('our_world.more_medals')}</span>
         </a>
       </div>
-      <div className={`flex justify-center items-center ml-10 ${lastOne && 'ml-[45rem]'}`}>
-        {!lastOne && <img src={arrowSlider} alt="arrow" className="2xl:w-fit w-14" />}
-      </div>
+      {!lastOne && (
+        <div className={`flex justify-center items-center ml-10 ${lastOne && 'ml-[45rem]'}`}>
+          <img src={arrowSlider} alt="arrow" className="2xl:w-fit w-14" />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/ourWorld/MedalsSlider.tsx
+++ src/components/ourWorld/MedalsSlider.tsx
@@ -7,7 +7,7 @@ export const MedalsSlider = () => {
   const timelineRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
   const { events } = useDraggable(timelineRef);
   return (
-    <div className="flex w-full overflow-x-scroll scrollbar-hide sm:ml-12 sm:mt-0 mt-10" {...events} ref={timelineRef}>
+    <div className="flex w-full overflow-x-scroll scrollbar-hide sm:m-12 sm:mt-0 mt-10" {...events} ref={timelineRef}>
       <div className="flex">
         {medalsData.map((medal, i) => (
           <MedalsItemSlider key={i} rumMedals={medal} lastOne={i === medalsData.length - 1} />
